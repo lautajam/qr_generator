158033c1bf1fad6fd91f0299f149fd3378cced9e
Lautaro <lautajamdev@gmail.com>
2023-06-19 01:25:31 -0300

    the save path window available and functional
    can select the save path button and type the new save path (just writes this in console)

---
+++ visual/functions.py
@@ -0,0 +1,34 @@
+# Show Qr_gen frame
+def qr_gen(main_frame, QR_gen_frame):
+    main_frame.pack_forget()
+    QR_gen_frame.pack()
+
+# Show save_path frame
+def save_path(main_frame, save_path_frame):
+    main_frame.pack_forget()
+    save_path_frame.pack()
+
+# Show extension frame
+def extension(main_frame, extension_frame):
+    main_frame.pack_forget()
+    extension_frame.pack()
+
+# Quit app
+def exit_app(root):
+    print("You left.")
+    root.quit()
+
+# Hide the current frame and show main frame
+def back_to_main_frame(frame, main_frame):
+    frame.pack_forget()
+    main_frame.pack()
+    print("Back to main menu")
+
+# Prints on console the content and the name of the qr entered.
+def create(content_qr_entry, name_qr_entry):
+    print("QR content:", content_qr_entry)
+    print("QR name:", name_qr_entry)
+
+# Prints on console the changed path
+def change_path(sp_path_entry):
+    print("The new path is:", sp_path_entry)
--- visual/main_visual.py
+++ visual/main_visual.py
@@ -1,40 +1,5 @@
 import tkinter as tk
-
-# Show Qr_gen frame
-def qr_gen():
-    print("QR generated.")
-    main_frame.pack_forget()
-    QR_gen_frame.pack()
-
-# Show save_path frame
-def save_path():
-    print("The save path is...")
-    main_frame.pack_forget()
-    save_path_frame.pack()
-
-# Show extension frame
-def extension():
-    print("The image extension is...")
-    main_frame.pack_forget()
-    extension_frame.pack()
-
-# Quit app
-def exit_app():
-    print("You left.")
-    root.quit()
-
-# Hidde the actual frame and show main frame
-def back_to_main_frame(frame):
-    frame.pack_forget()
-    main_frame.pack()
-    print("Back to main menu")
-
-# Prints on console the content and the name of the qr entered.
-def submit():
-    content_qr = content_qr_entry.get()
-    print("QR content:", content_qr)
-    name_qr = name_qr_entry.get()
-    print("QR name:", name_qr)
+from functions import *
 
 # Creates the main window
 root = tk.Tk()
@@ -44,31 +9,10 @@ root.geometry("700x300")
 # Creates the main frame
 main_frame = tk.Frame(root)
 
-# Labels main frame
-welcome_lbl = tk.Label(main_frame, text="Welcome to Qr Generator!")
-welcome_lbl.pack()
+#--- FRAMES ---#
 
-option_lbl = tk.Label(main_frame, text="Select your option.")
-option_lbl.pack()
 
-# Buttons main frame
-qr_gen_btn = tk.Button(main_frame, text="Qr generate", command=qr_gen)
-qr_gen_btn.pack()
-
-save_path_btn = tk.Button(main_frame, text="Select the save path", command=save_path)
-save_path_btn.pack()
-
-extension_btn = tk.Button(main_frame, text="Select the image extension", command=extension)
-extension_btn.pack()
-
-exit_app_btn = tk.Button(main_frame, text="Exit app", command=exit_app)
-exit_app_btn.pack()
-
-# FRAMES
-#
-# 
 # Creates the QR_gen_frame frame
-
 QR_gen_frame = tk.Frame(root)
 
 QR_gen_frame_label = tk.Label(QR_gen_frame, text="This is the QR_gen screen.")
@@ -84,30 +28,61 @@ name_qr_label.pack()
 name_qr_entry = tk.Entry(QR_gen_frame)
 name_qr_entry.pack()
 
-create_qr_bt = tk.Button(QR_gen_frame, text="Create Qr code", command=submit)
+create_qr_bt = tk.Button(QR_gen_frame, text="Create Qr code", command=lambda: create(content_qr_entry.get(), name_qr_entry.get()))
 create_qr_bt.pack()
 
-QR_gen_frame_button = tk.Button(QR_gen_frame, text="Back", command=lambda: back_to_main_frame(QR_gen_frame))
+QR_gen_frame_button = tk.Button(QR_gen_frame, text="Back", command=lambda: back_to_main_frame(QR_gen_frame, main_frame))
 QR_gen_frame_button.pack()
+
 
 
 # Creates the QR_gen_frame frame
 save_path_frame = tk.Frame(root)
 
-save_path_label= tk.Label(save_path_frame, text="This is the save_path screen.")
-save_path_label.pack()
+sp_title_label= tk.Label(save_path_frame, text="Your path now is:")
+sp_title_label.pack()
+sp_label1= tk.Label(save_path_frame, text="*PATH*")
+sp_label1.pack()
+sp_label2= tk.Label(save_path_frame, text="Please, type in the path to save the image")
+sp_label2.pack()
+sp_path_entry = tk.Entry(save_path_frame)
+sp_path_entry.pack()
+sp_path_bt = tk.Button(save_path_frame, text="Change save path", command=lambda: change_path(sp_path_entry.get()))
+sp_path_bt.pack()
 
-save_path_button = tk.Button(save_path_frame, text="Back", command=lambda: back_to_main_frame(save_path_frame))
+save_path_button = tk.Button(save_path_frame, text="Back", command=lambda: back_to_main_frame(save_path_frame, main_frame))
 save_path_button.pack()
 
-# Creates the QR_gen_frame frame
+"""
+# Creates the extension frame
 extension_frame = tk.Frame(root)
 
 extension_label= tk.Label(extension_frame, text="This is the extension screen.")
 extension_label.pack()
 
-extension_button = tk.Button(extension_frame, text="Back", command=lambda: back_to_main_frame(extension_frame))
+extension_button = tk.Button(extension_frame, text="Back", command=lambda: back_to_main_frame(extension_frame, main_frame))
 extension_button.pack()
+"""
+
+# Labels main frame
+welcome_lbl = tk.Label(main_frame, text="Welcome to Qr Generator!")
+welcome_lbl.pack()
+
+option_lbl = tk.Label(main_frame, text="Select your option.")
+option_lbl.pack()
+
+# Buttons main frame
+qr_gen_btn = tk.Button(main_frame, text="Qr generate", command=lambda: qr_gen(main_frame, QR_gen_frame))
+qr_gen_btn.pack()
+
+save_path_btn = tk.Button(main_frame, text="Select the save path", command=lambda: save_path(main_frame, save_path_frame))
+save_path_btn.pack()
+
+extension_btn = tk.Button(main_frame, text="Select the image extension",  command=lambda: extension(main_frame, extension_frame))
+extension_btn.pack()
+
+exit_app_btn = tk.Button(main_frame, text="Exit app", command=lambda: exit_app(root))
+exit_app_btn.pack()
 
 # Show main frame at start and start the app loop
 main_frame.pack()
